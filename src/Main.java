747ecb719d2b775dd1367a5dfa96d81cfee9fb53
lorenamejiag <loregalvan0930@gmail.com>
2025-04-30 20:02:46 -0500

    otros cambios

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
+        for (int I=0; I < 10; I++);
+        System.out.println();
 
     }
 }
